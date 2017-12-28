ceae4288815cf59adc59b5b649cc2a375e93efd9
Fredrik <fredrik.k.andersson@gmail.com>
2017-12-28 21:53:52 +0100

    fixed player stayed imortal forever (even if plugin was deleted)

--- src/main/java/com/yellowfortyfour/spigot/nodamage/HealthListener.java
+++ src/main/java/com/yellowfortyfour/spigot/nodamage/HealthListener.java
@@ -5,20 +5,28 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class HealthListener implements Listener
 {
 	@EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) 
+    public void onPlayerJoin(PlayerJoinEvent event)
 	{
 		Player player = event.getPlayer();
-		
+
 		Bukkit.getLogger().info("Player " + player.getDisplayName() + " joined, setting imortality.");
-		
+
 		player.setInvulnerable(true);
-		
+
 		if(!player.isOp())
 			player.setCanPickupItems(false);
 	}
-}
 
+	@EventHandler
+		public void onPlayerLeave(PlayerQuitEvent event)
+		{
+			Player player = event.getPlayer();
+			player.setInvulnerable(false);
+			player.setCanPickupItems(true);
+		}
+}
